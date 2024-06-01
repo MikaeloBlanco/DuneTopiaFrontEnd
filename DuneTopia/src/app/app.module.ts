a4b7a1229773455a6622bf6d4ab503a17870060f
MikaeloBlanco <mikaeloescritor@gmail.com>
2024-06-01 20:57:49 +0200

    Componentes

--- DuneTopia/src/app/app.module.ts
+++ DuneTopia/src/app/app.module.ts
@@ -4,21 +4,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CabezaComponent} from './Cabeza/header.component'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { PiePaginaComponent } from './Pie-Pagina/pie-pagina.component';
 
 @NgModule({
-    declarations:[
-        AppComponent
+    declarations: [
+        AppComponent,
+        
     ],
+    providers: [],
+    bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
-        ReactiveFormsModule
-    ],
-    providers: [],
-    bootstrap:[
-        AppComponent
+        ReactiveFormsModule,
+        HttpClientModule,
+        HeaderComponent,
+        PiePaginaComponent
     ]
 })
 export class AppModule {}
